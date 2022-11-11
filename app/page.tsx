3d3fd2f64c854648d37c98f3739a3fb8cd8fe04c
Micha Buchholz <mail@lean-stack.de>
2022-11-11 22:14:20 +0100

    Explore env vars.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function HomePage() {
-  return <h1>Weltspiegel Cottbus</h1>
+async function getData() {
+  const data = process.env.MANDATOR_ID;
+  return data;
+}
+
+export default async function HomePage() {
+  const data = await getData();
+
+  return (
+    <div>
+      <h1>Weltspiegel Cottbus</h1>
+      <pre>{data}</pre>
+    </div>
+  );
 }
